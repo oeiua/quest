a6a875383142ed04af8c76231ea97564f5b1af59
Anton Tsekhmistro <anton.tsekh@gmail.com>
2023-09-20 20:10:59 +0300

    create startgame func

--- internal/pkg/quest/quest.go
+++ internal/pkg/quest/quest.go
@@ -120,3 +120,56 @@ func PickupItem(maze Maze) Maze {
 	maze.Item = nil
 	return maze
 }
+
+func StartGame(){
+	fmt.Printf("Start you journey from here\n")
+	fmt.Printf("f/b/r/l - to move forward, back, right, left\n")
+	fmt.Printf("p - to pickup item from ground\n")
+	fmt.Printf("o - to look around\n")
+	fmt.Printf("s - to get status of hero\n")
+
+	var action string
+	hero := InitHero()
+	maze := InitMaze(&hero)
+	game := true
+	for game {
+
+		fmt.Scanf("%s", &action)
+
+		switch action {
+		case "left", "l":
+			if maze.CanMove("left") {
+				maze = MoveLeft(maze)
+			}
+		case "right", "r":
+			if maze.CanMove("right") {
+				maze = MoveRight(maze)
+			}
+		case "back", "b":
+			if maze.CanMove("back") {
+				maze = MoveBack(maze)
+			}
+		case "forward", "f":
+			if maze.CanMove("forward") {
+				maze = MoveForward(maze)
+			}
+		case "pickup", "p":
+			if maze.Item != nil {
+				maze = PickupItem(maze)
+			}
+		case "status", "s":
+			hero.GetStats()
+		case "observe", "o":
+			Observe(&maze)
+		case "suicide":
+			game = false
+		}
+
+		action = ""
+
+		if !hero.IsAlive() {
+			game = false
+		}
+	}
+	fmt.Print("Game over!")
+}
